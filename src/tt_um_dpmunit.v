39980a9ade924500c7279913039b8f5df6b5c65e
RVCE-ECE-Shylashree <drshylashreen@gmail.com>
2024-09-06 22:53:31 +0530

    Update and rename tt_um_dpmu.v to tt_um_dpmunit.v

--- src/tt_um_dpmunit.v
+++ src/tt_um_dpmunit.v
@@ -1,5 +1,5 @@
 `default_nettype none
-module tt_um_dpmu(
+module tt_um_dpmunit(
      input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
